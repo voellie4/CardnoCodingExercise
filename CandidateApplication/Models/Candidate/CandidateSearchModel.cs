f5600c46196b11981b2d9c6e9f41fe08fcad649d
Ellie Vo <vo.ellie@gmail.com>
2019-06-06 12:05:26 -0500

    Add files via upload

---
+++ CandidateApplication/Models/Candidate/CandidateSearchModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CandidateApplication.Models.Candidate
+{
+    public class CandidateSearchModel
+    {
+        public string SchFirst { get; set; }
+        public string SchLast { get; set; }
+        public string SchPhone { get; set; }
+        public string SchEmail { get; set; }
+        public string SchZipCode { get; set; }
+        public string SchQuaType { get; set; }
+        public string SchQuaName { get; set; }
+        public Int32 TotalRecords { get; set; }
+        [Display(Name = "Search Date")]
+        public DateTime? SchDate { get; set; }
+        public List<string> SchQTypes { get; set; }
+        public List<string> SchQNames { get; set; }
+        public List<Candidate> Candidates { get; set; }
+    }
+}
